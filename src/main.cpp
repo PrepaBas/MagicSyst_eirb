0eaa295afe0896970a082118148ff367b771e748
Bastien <btranruesch@gmail.com>
2025-04-07 13:46:45 +0200

    "ca marche"

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,9 @@ void setup() {
   set_x(100);
   set_y(865);
   set_theta(0);
+  begin_steppers();
+  wheelRadius = 72.8/2;
+  baseWidth = 256.;
   enable_steppers();
   stepper_param.max_speed = 10000;
   int robot_stop = 0;
@@ -100,14 +103,14 @@ void dispatchTaskcode(void* parameters) {
 
 
 void securityTaskcode(void* parameters){
-  vTaskDelay(pdMS_TO_TICKS(4500));
+  /* vTaskDelay(pdMS_TO_TICKS(4500));
   int* robot_stop_ptr = (int*)parameters;
   *robot_stop_ptr = 1;
   vTaskDelete(currentTask);
   remaining_steps = 200;
   new_position();
   vTaskDelay(pdMS_TO_TICKS(5000));
-  *robot_stop_ptr = 0;
+  *robot_stop_ptr = 0; */
   for(;;){
     vTaskDelay(pdMS_TO_TICKS(10));
     //robot.motors.param.max_speed = robot.motors.param.min_speed+(ms++%50000);
