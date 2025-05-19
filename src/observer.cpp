97aebc4a3f162af1f92d207c9044c54acf1e3207
Bastien <btranruesch@gmail.com>
2025-05-19 16:57:58 +0200

    no restart

--- src/observer.cpp
+++ src/observer.cpp
@@ -9,6 +9,8 @@
 #define SPEED_OF_SOUND 340
 #define STEPS_TO_STOP 200
 
+#define STEPS_TO_STOP 200
+
 
 uint32_t stop_robot(int *robot_stop_ptr)
 {
@@ -42,8 +44,30 @@ void securityTaskcode(void *parameters)
   NewPing rl(17, 17, 200); // rear left
   NewPing cn(17, 17, 200); // cans near
   NewPing cf(17, 17, 200); // cans far
+{   // 21, 19, 18, 5, 17, 16, 4
+  NewPing fc(17, 17, 200); // front center
+  NewPing fr(19, 19, 200); // front right
+  NewPing fl(5, 5, 200); // front left
+  NewPing rr(17, 21, 200); // rear right
+  NewPing rl(17, 17, 200); // rear left
+  NewPing cn(17, 17, 200); // cans near
+  NewPing cf(17, 17, 200); // cans far
 
   int *robot_stop_ptr = (int *)parameters;
+
+  status_t status = NO_STOP;
+
+  unsigned long fc_distance;
+  unsigned long fr_distance;
+  unsigned long fl_distance;
+  unsigned long rl_distance;
+  unsigned long rr_distance;
+  unsigned long cn_distance;
+  unsigned long cf_distance;
+  uint32_t cut_steps = 0;
+  bool detect;
+  unsigned long stop_timer;
+
 
   status_t status = NO_STOP;
 
@@ -79,36 +103,20 @@ void securityTaskcode(void *parameters)
         if(fc_distance<FRONT_EMPTY_STOP_DISTANCE) detect = 1;
         break;
 
-      case LOADED_COMMUTE:
-      // front right sensor
-        fr_distance = fr.ping_cm();
-        if(!fr_distance) fr_distance = 200; 
-        if(fr_distance<FRONT_LOADED_STOP_DISTANCE) detect = 1;
-
-        // front right sensor
-        fl_distance = fl.ping_cm();
-        if(!fl_distance) fl_distance = 200; 
-        if(fl_distance<FRONT_LOADED_STOP_DISTANCE) detect = 1;
-
-        // center right sensor
-        fl_distance = fl.ping_cm();
-        if(!fc_distance) fc_distance = 200; 
-        if(fc_distance<FRONT_LOADED_STOP_DISTANCE) detect = 1;
-        break;
-
-      case EMPTY_APPROACH:
-      break;
-      case LOADED_APPROACH:
-      break;
-      case BACKING:
-      break;
-      case NO_SECURITY:
-      break;
+      // case LOADED_COMMUTE:
+      // break;
+      // case EMPTY_APPROACH:
+      // break;
+      // case LOADED_APPROACH:
+      // break;
+      // case BACKING:
+      // break;
+      // case NO_SECURITY:
+      // break;
     }
     switch(status){
       case NO_STOP:
         if(detect){
-          Serial.println("stopping robot");
           uint32_t cut_steps = stop_robot(robot_stop_ptr);
           stop_timer = esp_timer_get_time();
           status = YES_STOP;
@@ -120,7 +128,6 @@ void securityTaskcode(void *parameters)
           stop_timer = esp_timer_get_time();
         }
         else if(esp_timer_get_time() - stop_timer > 3000000){
-          Serial.println("resuming robot");
           resume_robot(robot_stop_ptr, cut_steps);
           status = NO_STOP;
         }
